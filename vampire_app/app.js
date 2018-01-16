624227426ea811c964b6d1674c5f3688442c1ba4
Anthony <anthonyjlower@gmail.com>
2018-01-15 22:14:42 -0600

    Commit 5 - selected more vampires

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -204,6 +204,60 @@ mongoose.connection.on("error", (error) => {
 /////////////////////////////////////////////////
 // ### Select with OR
 
+// are from New York, New York, US or New Orleans, Louisiana, US
+
+// Vampire.find({ $or: 
+// 	[ {location: "New York, New York, US"},
+// 	{location: "New Orleans, Louisiana, US"}]
+// }, (err, foundVamps) => {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(foundVamps)
+// 	}
+// })
+
+
+// love brooding or being tragic
+
+// Vampire.find({ $or:
+// 	[ {loves: "brooding"},
+// 	{loves: "being tragic"}]
+// }, (err, foundVamps) => {
+// 	if (err) {
+// 		console.log(err)
+// 	} else {
+// 		console.log(foundVamps)
+// 	}
+// })
+
+// have more than 1000 victims or love marshmallows
+// Vampire.find({ $or:
+// 	[ {victims: {$gt: 1000}},
+// 	{loves: "marshmallows"}]
+// }, (err, foundVamps) => {
+// 	if (err) {
+// 		console.log(err)
+// 	} else {
+// 		console.log(foundVamps)
+// 	}
+// })
+
+
+// have red hair or green eyes
+Vampire.find({ $or:
+	[ { hair_color: "red"},
+	{ eye_color: "green"}]
+}, (err, foundVamps) => {
+	if (err) {
+		console.log(err)
+	} else {
+		console.log(foundVamps)
+	}
+})
+
+
+
 /////////////////////////////////////////////////
 //### Select objects that match one of several values
 
